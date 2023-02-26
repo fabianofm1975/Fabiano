70cafd15fcdd3b2ba0f7b7a370dfee89686a4b1b
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-02-26 18:02:32 -0300

    incluir tfstate

--- Infra/main.tf
+++ Infra/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.36.0"
     }
   }
+  backend "azurerm" {
+        resource_group_name  = "Automacao"
+        storage_account_name = "automacaoterraform"
+        container_name       = "terraform"
+        key                  = "terraform.tfstate"
+    }
 }
 
 provider "azurerm" {
