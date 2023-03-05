d1c5d4c072d8671746e8e60c1f488b4af6eea913
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-03-05 20:12:43 -0300

    fw

--- Infra/network.tf
+++ Infra/network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_postgresql_flexible_server_firewall_rule" "pg-automacao-fw" {
-  name             = "fabiano-fw"
+  name             = "Open-fw"
   server_id        = azurerm_postgresql_flexible_server.pg-automacao.id
-  start_ip_address = "177.124.63.14"
-  end_ip_address   = "177.124.63.14"
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
 }
