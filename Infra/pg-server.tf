2c4bd8300866baa4338fe78868b07f734c05558d
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-03-06 20:09:01 -0300

    desfazendo ajust

--- Infra/pg-server.tf
+++ Infra/pg-server.tf
@@ -1,5 +1,5 @@
-resource "azurerm_postgresql_flexible_server" "pg_automacao" {
-  name                          = "pg_automacao"
+resource "azurerm_postgresql_flexible_server" "pg-automacao" {
+  name                          = "pg-automacao"
   resource_group_name           = azurerm_resource_group.rg.name
   location                      = azurerm_resource_group.rg.location
   version                       = "14"
