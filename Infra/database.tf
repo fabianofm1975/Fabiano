b6fa34b6f44a877242b0e7580ff47b2a4b0ea5ca
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-03-06 20:02:05 -0300

    alterações nos databases

--- Infra/database.tf
+++ Infra/database.tf
@@ -1,22 +1,19 @@
-resource "azurerm_postgresql_flexible_server_database" "db-automacao" {
-  name      = "db-automacao"
+resource "azurerm_postgresql_flexible_server_database" "db_automacao" {
+  name      = "db_automacao"
   server_id = azurerm_postgresql_flexible_server.pg-automacao.id
   collation = "en_US.utf8"
   charset   = "utf8"
 }
 
-resource "azurerm_postgresql_flexible_server_database" "db-apresentacao" {
-  name      = "db-apresentacao"
+resource "azurerm_postgresql_flexible_server_database" "db_presentation" {
+  name      = "db_presentation"
   server_id = azurerm_postgresql_flexible_server.pg-automacao.id
   collation = "en_US.utf8"
   charset   = "utf8"
 }
-
 
 resource "azurerm_postgresql_flexible_server_configuration" "pg-automacao-cfg" {
   name      = "azure.extensions"
   server_id = azurerm_postgresql_flexible_server.pg-automacao.id
   value     = "PG_STAT_STATEMENTS,PG_BUFFERCACHE,PG_FREESPACEMAP,PG_REPACK,PAGEINSPECT,PG_VISIBILITY,PGSTATTUPLE,PG_PREWARM"
-
 }
-
