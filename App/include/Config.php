191e461a212be79892bb4356ba14df14ee1c9b47
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-03-12 18:09:24 -0300

    clean up

--- App/include/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Database config variables
- */
-define("DB_HOST", "localhost");
-define("DB_USER", "malayeri");
-define("DB_PASSWORD", "123456789");
-define("DB_DATABASE", "malayeri");
