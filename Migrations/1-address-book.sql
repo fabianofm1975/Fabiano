46de8cce8cb74a4d99216bcc5b73ba26bdbea789
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-03-12 19:14:05 -0300

    table

--- Migrations/1-address-book.sql
+++ Migrations/1-address-book.sql
@@ -1,8 +1,8 @@
-CREATE TABLE 'address_book' (
-  'id' bigserial NOT NULL primary key,
-  'name' varchar(255) NOT NULL,
-  'email' varchar(255) NOT NULL,
-  'tel' varchar(255) NOT NULL,
-  'address' text NOT NULL
+CREATE TABLE address_book (
+  id serial NOT NULL primary key,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  tel varchar(255) NOT NULL,
+  address text NOT NULL
 );
 
